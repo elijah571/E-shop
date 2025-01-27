5587e19f2d666892e67192a382eee776d29e8dd6
elijah571 <elijahfx43@gmail.com>
2025-01-27 14:39:01 +0100

    navbar fixed

--- UI/src/pages/Products/FavoritesCount.jsx
+++ UI/src/pages/Products/FavoritesCount.jsx
@@ -5,7 +5,7 @@ const FavoritesCount = () => {
   const favoriteCount = favorites.length;
 
   return (
-    <div className="absolute left-2 top-8">
+    <div className="absolute left-1 -top-4">
       {favoriteCount > 0 && (
         <span className="px-1 py-0 text-sm text-white bg-pink-500 rounded-full">
           {favoriteCount}

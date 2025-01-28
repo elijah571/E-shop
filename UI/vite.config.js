713d64c630acf1478ea7ffc22384edf687e39855
elijah571 <elijahfx43@gmail.com>
2025-01-29 00:18:15 +0100

    files changed

--- UI/vite.config.js
+++ UI/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api/": "https://e-shop-cdhd.onrender.com", 
-      "/uploads/": "http://localhost:5000",
+      "/uploads/": "https://e-shop-cdhd.onrender.com",
     },
   },
 });
